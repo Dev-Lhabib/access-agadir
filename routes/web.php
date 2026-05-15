c9179f01d8a89b57cf4383991533fb404b89960f
Dev-Lhabib <elhabibmahfoud@gmail.com>
2026-05-15 13:02:03 -0700

    feat: add reviews, route button, and mini-map to place detail

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MapController;
 use App\Http\Controllers\ObstacleController;
 use App\Http\Controllers\PlaceController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
@@ -11,4 +12,5 @@ Route::get('/map', [MapController::class, 'index'])->name('map.index');
 
 Route::get('/api/places', [PlaceController::class, 'index'])->name('api.places');
 Route::get('/places/{id}', [PlaceController::class, 'show'])->name('places.show');
+Route::post('/places/{id}/reviews', [ReviewController::class, 'store'])->name('places.reviews.store');
 Route::get('/api/obstacles', [ObstacleController::class, 'index'])->name('api.obstacles');
